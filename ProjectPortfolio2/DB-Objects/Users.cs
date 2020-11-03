bf40935fc6057e042746ff59b9a088752558d494
Ali Hassan Raza <ahraza@ruc.dk>
2020-11-03 12:56:28 +0100

    class Users

---
+++ ProjectPortfolio2/DB-Objects/Users.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectPortfolio2
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Language { get; set; }
+        public string Mail { get; set; }
+    }
+}
