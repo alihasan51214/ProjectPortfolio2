da4210638c5fc09c40960659597b053fbfdd1efd
112lenca112 <72919798+112lenca112@users.noreply.github.com>
2020-11-05 14:45:06 +0100

    * Program.cs: * DataService.cs: * Npgsql.dll: * ProjectPortfolio2.csproj.nuget.g.props: * Microsoft.Bcl.HashCode.dll: * System.Collections.Immutable.dll: * Microsoft.Extensions.Logging.dll: * Microsoft.Extensions.Options.dll: * Microsoft.EntityFrameworkCore.dll: * Microsoft.Bcl.AsyncInterfaces.dll: * Microsoft.Extensions.Primitives.dll: * Microsoft.Extensions.Configuration.dll: * Microsoft.Extensions.Caching.Memory.dll: * System.Diagnostics.DiagnosticSource.dll: * Npgsql.EntityFrameworkCore.PostgreSQL.dll: * Microsoft.Extensions.DependencyInjection.dll: * Microsoft.Extensions.Logging.Abstractions.dll: * Microsoft.Extensions.Configuration.Binder.dll: * Microsoft.EntityFrameworkCore.Abstractions.dll: * Microsoft.Extensions.Configuration.Abstractions.dll: * Microsoft.Extensions.DependencyInjection.Abstractions.dll:
    * Microsoft.Extensions.Caching.Abstractions.dll: g

--- ProjectPortfolio2/Program.cs
+++ ProjectPortfolio2/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ProjectPortfolio2
 {
     class Program
     {
         static void Main(string[] args)
-        {    
-            var dataService = new DataService();
+        {
+            var config = new ConfigurationBuilder()
+            .AddJsonFile("config.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+            var dataService = new DataService(config["connectionString"]);
+
+
 
             foreach (var elem in dataService.GetActorsKnownForTitles())
             {
